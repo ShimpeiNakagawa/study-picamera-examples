69ad085253b230a835c4342c6632d4eea11a8b40
ShimpeiNakagawa <35630217+ShimpeiNakagawa@users.noreply.github.com>
2018-07-23 20:48:09 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -43,7 +43,7 @@ class FaceDetector(object):
             i += 1
             
         font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(frame,str(i),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame,str(i),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
         
         # frameを戻り値として返す
         return frame
